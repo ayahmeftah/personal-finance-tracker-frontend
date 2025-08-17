ced1ae5ac86a806d5e32a37c602a4bbed603be83
MalakAlmajed15 <malakalmajed15@gmail.com>
2025-08-17 10:48:55 +0300

    importing all the components in the navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import CategoryLink from './CategoryLink'
+import LogoutLink from './LogoutLink'
+import ProfileLink from './ProfileLink'
+import TransactionsLink from './TransactionsLink'
+import UserInformation from './UserInformation'
 
 const NavBar = () => {
   return (
     <div>
-      
+
     </div>
   )
 }
