82eeb0e22b065f8f303ad224eac3572bd96338c9
ayahmeftah <ayahmeftah123123@gmail.com>
2025-08-18 09:35:24 +0300

    added app from template

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,48 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router'
+import SignUp from './components/SignUpForm/SignUpForm'
+import Dashboard from './components/Dashboard/Dashboard'
+
 
 const App = () => {
+  const [token, setToken] = useState(localStorage.getItem('token'))
+
+  function handleLogin(newToken) {
+    setToken(newToken)
+  }
+
+  function handleLogout() {
+    setToken(null)
+    localStorage.removeItem('token')
+  }
+
+  // This is how to decode the token and gget the 
+  // information that you added to the payload in your login 
+  // route in the backend
+  if (token) {
+    const decodedToken = jwtDecode(token)
+    console.log(decodedToken)
+  }
+
   return (
-    <>
-      
-    </>
+    <Router>
+      <div>
+        {token ? <LogoutButton onLogout={handleLogout} /> : null}
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/login" element={<LoginForm onLogin={handleLogin} />} />
+          <Route path="/signup" element={<SignUp />} />
+          {/* <Route
+            path="/pets"
+            element={
+              <ProtectedRoute>
+                <Pets />
+              </ProtectedRoute>
+            }
+          /> */}
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
