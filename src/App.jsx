4aeb7de985d664e94fcbf5c3c27cc5272d2aca61
MalakAlmajed15 <malakalmajed15@gmail.com>
2025-08-18 22:13:37 +0300

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -54,7 +54,7 @@ const App = () => {
           <Route path="/signup" element={<SignUp onSignUp={handleLogin} />} />
           <Route path="/" element={
             <ProtectedRoute>
-              <CategoryForm editCategory={editCategory}/>
+              {/* <CategoryForm editCategory={editCategory}/> */}
               {/* <Dashboard /> */}
               <CategoryList handleEditClick={handleEditClick}/>
               {/* <EditCategoryButton /> */}
--- src/components/Category/DeleteCategoryButton/DeleteCategoryButton.jsx
+++ src/components/Category/DeleteCategoryButton/DeleteCategoryButton.jsx
@@ -2,6 +2,8 @@ import categoryCalls from '../../../../lib/category-api'
 
 const DeleteCategoryButton = () => {
     return (
+        <>
         
+        </>
     )
 }
