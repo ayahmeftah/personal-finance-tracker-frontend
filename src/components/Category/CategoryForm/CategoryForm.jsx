ff4e0dd4107523f78cd091a0360224713dbc84eb
MalakAlmajed15 <malakalmajed15@gmail.com>
2025-08-18 19:25:39 +0300

    fixing some errors

--- src/components/Category/CategoryForm/CategoryForm.jsx
+++ src/components/Category/CategoryForm/CategoryForm.jsx
@@ -1,68 +1,58 @@
 import categoryCalls from '../../../../lib/category-api'
-import {useState, useEffect} from 'react'
+import {useState} from 'react'
 
 const CategoryForm = () => {
-    // const [categoryToEdit, SetCategoryToEdit] = useState(null)
-    
+
     const [formData, setFormData] = useState({
         name: '',
         type: ''
     })
 
-    // useEffect(() => {
-    //     if (categoryToEdit) {
-    //         setFormData({
-    //             name: categoryToEdit.name || '',
-    //             type: categoryToEdit.type || ''
-    //         })
-    //     }
-    // }, [categoryToEdit])
-    
     const [isSubmitting, setIsSubmitting] = useState(false)
 
     const handleFormChange = (event) => {
-        event.preventDefault()
         setFormData({...formData, [event.target.name]: event.target.value})
     }
 
     const handleSubmitForm = async (event) => {
         event.preventDefault()
-        if (isSubmitting) return 
-        setIsSubmitting(true)
-        let response = await categoryCalls.createCategory(formData) 
-        // if (categoryToEdit && categoryToEdit._id) {
-        //     response = await categoryCalls.updateCategory(categoryToEdit._id, formData)
-        // } else {
-        //     response = await categoryCalls.createCategory(formData)
-        // }
-
-        if (response.status === 201 || response.status === 200) {
-            setFormData({
-                name: '',
-                type: ''
-            })   
+        // let response = await categoryCalls.createCategory(formData) 
+        // if (response.status === 201 || response.status === 200) {
+        //     setFormData({
+        //         name: '',
+        //         type: ''
+        //     })   
+        //     }
+        if (!formData.name || !formData.type) {
+            prompt('Please fill all fields')
         }
-        setIsSubmitting(false)
     } 
 
-    
-    
+
     return (
        <>
-       {/* <h1>{categoryToEdit && categoryToEdit._id ? 'Edit Category' : 'Add Category'}</h1> */}
        <h1>Add Category</h1>
         <form onSubmit={handleSubmitForm} >
         <label htmlFor="name">Category Name: </label>
-        <input className='name' id='name' value={formData.name} onChange={handleFormChange}></input>
-        <label htmlFor="type">Category Type: </label>
-            <select id='select-category-type' value={formData.type} onChange={handleFormChange}>
-                <option value="">Select type</option>
+        <input 
+        name='name' 
+        id='name' 
+        value={formData.name} 
+        onChange={handleFormChange} 
+        />
+        <label htmlFor="select-category-type">Category Type: </label>
+            <select 
+            name='type' 
+            id='select-category-type' 
+            value={formData.type}
+            onChange={handleFormChange}
+            >
+                <option value=''>Select type</option>
                 <option value='income'>Income</option>
                 <option value='expense'>Expense</option>
             </select>
+            <button type='submit'>Add</button>
         </form>
-        {/* <button>{categoryToEdit && categoryToEdit._id ? 'update' : 'add'}</button> */}
-        <button>Add</button>
        </> 
     ) 
 }
