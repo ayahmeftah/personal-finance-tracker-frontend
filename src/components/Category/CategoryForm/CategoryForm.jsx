81e49edc7e26858678f593ea14b38bb184f12e53
MalakAlmajed15 <malakalmajed15@gmail.com>
2025-08-18 13:41:13 +0300

    adding some code to category form

--- src/components/Category/CategoryForm/CategoryForm.jsx
+++ src/components/Category/CategoryForm/CategoryForm.jsx
@@ -1,19 +1,50 @@
 import categoryCalls from '../../../../lib/category-api'
+import {useState, useEffect} from 'react'
 
 const CategoryForm = () => {
-    const handleSubmitForm = () => {
-        
+    const [categoryToEdit, SetCategoryToEdit] = useState(null)
+    
+    const [formData, setFormData] = useState({
+        name: '',
+        type: ''
+    })
+
+    useEffect(() => {
+        if (categoryToEdit) {
+            setFormData({
+                name: categoryToEdit.name || '',
+                type: categoryToEdit.type || ''
+            })
+        }
+    }, [categoryToEdit])
+    
+    const [isSubmitting, setIsSubmitting] = useState(false)
+
+    const handleFormChange = (event) => {
+        event.preventDefault()
+        setFormData({...formData, [event.target.name]: event.target.value})
+    }
+
+    const handleSubmitForm = async (event) => {
+        event.preventDefault()
+        if (isSubmitting) return 
     } 
     
     return (
        <>
+       <h1>{categoryToEdit && categoryToEdit._id ? 'Edit Category' : 'Add Category'}</h1>
         <form onSubmit={handleSubmitForm} >
-        <label htmlFor="name"></label>
-        <input></input>
-        <label htmlFor="type"></label>
-        <input></input>
+        <label htmlFor="name">Category Name: </label>
+        <input class='name' id='name' value={formData.name}></input>
+        <label htmlFor="type">Category Type: </label>
+        <input class='type' id='type' value={formData.title}>
+            <select>
+                <option></option>
+                <option></option>
+            </select>
+        </input>
         </form>
-        <button onClick={}></button>
+        <button>{categoryToEdit && categoryToEdit._id ? 'update' : 'add'}</button>
        </> 
     ) 
 }
