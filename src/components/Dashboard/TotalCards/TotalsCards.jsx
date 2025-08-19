d84dcd49188750cdfd2ba1dbef1a1659deee1501
ayahmeftah <ayahmeftah123123@gmail.com>
2025-08-19 21:49:02 +0300

    added css to the total cards and added icons and modified index.css

--- src/components/Dashboard/TotalCards/TotalsCards.jsx
+++ src/components/Dashboard/TotalCards/TotalsCards.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import transactionsCalls from '../../../../lib/transaction-api'
+import './TotalCards.css'
+import WalletOutlinedIcon from '@mui/icons-material/WalletOutlined';
+import PaidOutlinedIcon from '@mui/icons-material/PaidOutlined';
+import AccountBalanceOutlinedIcon from '@mui/icons-material/AccountBalanceOutlined';
 
 const TotalsCards = () => {
 
@@ -39,14 +43,17 @@ const TotalsCards = () => {
   return (
     <div className="total-cards">
       <div className="card balance-card">
+        <div className="icon"><WalletOutlinedIcon sx={{ fontSize: 60 }}/></div>
         <h3>Total Balance</h3>
         <p>${totals.balance.toFixed(2)}</p>
       </div>
       <div className="card income-card">
+        <div className="icon"><PaidOutlinedIcon sx={{ fontSize: 60 }}/></div>
         <h3>Total Income</h3>
         <p>${totals.income.toFixed(2)}</p>
       </div>
       <div className="card expenses-card">
+        <div className="icon"><AccountBalanceOutlinedIcon sx={{ fontSize: 60 }}/></div>
         <h3>Total Expenses</h3>
         <p>${totals.expenses.toFixed(2)}</p>
       </div>
