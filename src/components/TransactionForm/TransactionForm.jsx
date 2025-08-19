256b180b67c24afc8f5f677fb2cf72147a3dad43
ayahmeftah <ayahmeftah123123@gmail.com>
2025-08-19 05:05:53 +0300

    started with transaction form and passed transactio type to it

---
+++ src/components/TransactionForm/TransactionForm.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import transactionsCalls from "../../../lib/transaction-api"
+import categoriesCalls from "../../../lib/category-api"
+
+const TransactionForm = ({ transactionType }) => {
+
+    const [formData, setFormData] = useState({
+        name: "",
+        amount: "",
+        categoryId: "",
+        date: "",
+        transactionType: transactionType,
+    })
+
+    return (
+        <div className='main-content'>
+            <form onSubmit={handleSubmit}>
+                <input
+                    name="name"
+                    value={formData.name}
+                    onChange={handleChange}
+                    placeholder="Transaction name"
+                    required
+                />
+                <input
+                    name="amount"
+                    type="number"
+                    value={formData.amount}
+                    onChange={handleChange}
+                    placeholder="Amount"
+                    required
+                />
+                <input
+                    name="date"
+                    type="date"
+                    value={formData.date}
+                    onChange={handleChange}
+                    required
+                />
+                <div style={{ display: "flex", gap: "8px" }}>
+                    <select
+                        name="categoryId"
+                        value={formData.categoryId}
+                        onChange={handleChange}
+                        required>
+                        <option value="">Select Category</option>
+                        {categories.map((category) => (
+                            <option key={category._id} value={category._id}>{category.name}</option>
+                        ))}
+                    </select>
+
+                </div>
+
+                <Button type="submit">Add {transactionType}</Button>
+            </form>
+        </div>
+    )
+}
+
+export default TransactionForm
