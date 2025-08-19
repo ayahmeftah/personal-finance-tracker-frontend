fb727f390e6350fc71d51fccf26004e7a8968266
ayahmeftah <ayahmeftah123123@gmail.com>
2025-08-19 22:42:11 +0300

    created state variable for chart data and fetch transactions

--- src/components/Dashboard/DashboardChart/DashboardChart.jsx
+++ src/components/Dashboard/DashboardChart/DashboardChart.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
+import transactionsCalls from '../../../../lib/transaction-api'
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts'
+import { useState } from 'react'
 
 const DashboardChart = () => {
+  const [chartData, setChartData] = useState([])
+
+  const getChartData = async () => {
+    const allTransactions = await transactionsCalls.getAllTransactions()
+    
+  }
+
   return (
     <div>
       
