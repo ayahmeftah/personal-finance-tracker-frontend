ecd9401b920788f33f8c42dc912dc85d57349225
ayahmeftah <ayahmeftah123123@gmail.com>
2025-08-19 21:58:48 +0300

    created state for transactions with function giving sorted 6 ones by date

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -4,6 +4,7 @@ import RecentTransactions from './RecentTransactions/RecentTransactions'
 import DashboardChart from './DashboardChart/DashboardChart'
 
 const Dashboard = () => {
+  
   return (
     <div className="main-content">
       <h1>Dashboard</h1>
--- src/components/Dashboard/RecentTransactions/RecentTransactions.jsx
+++ src/components/Dashboard/RecentTransactions/RecentTransactions.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import transactionsCalls from '../../../../lib/transaction-api'
+import { useState, useEffect } from 'react'
 
 const RecentTransactions = () => {
+
+  const [transactions, setTransactions] = useState([])
+
+  const getTransactionsSorted = async () => {
+    const allTransactions = await transactionsCalls.getAllTransactions();
+    if (!allTransactions.error) {
+      const recent = allTransactions.sort((a, b) => new Date(b.date) - new Date(a.date)).slice(0, 6)
+      setTransactions(recent)
+    }
+  }
+
+  useEffect(()=>{
+    getTransactionsSorted()
+  },[])
+
   return (
     <div>
-      
+
     </div>
   )
 }
