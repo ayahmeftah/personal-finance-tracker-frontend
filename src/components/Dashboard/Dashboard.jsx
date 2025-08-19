bb692d6c72ef120f8fcbcd831e8a76122e52852f
ayahmeftah <ayahmeftah123123@gmail.com>
2025-08-19 21:53:44 +0300

    created all component files needed in the dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import TotalCards from './TotalCards/TotalsCards'
-import RecentTransactions from './RecentTransactions'
-import DashboardChart from './DashboardChart'
+import RecentTransactions from './RecentTransactions/RecentTransactions'
+import DashboardChart from './DashboardChart/DashboardChart'
 
 const Dashboard = () => {
   return (
     <div className="main-content">
       <h1>Dashboard</h1>
       <TotalCards />
+      <RecentTransactions />
     </div>
   )
 }
