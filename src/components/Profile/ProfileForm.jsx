41a0a09815dd10c1707a2da5aaaf99ae39382c7f
MalakAlmajed15 <malakalmajed15@gmail.com>
2025-08-20 16:33:52 +0300

    adding some css to the profile form

--- src/components/Profile/ProfileForm.jsx
+++ src/components/Profile/ProfileForm.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router'
 import userCalls from '../../../lib/user-api'
-import './Profile.css'
+import './ProfileForm.css'
 const ProfileForm = ({ userId }) => {
     const navigate = useNavigate()
     const [currentUser, setCurrentUser] = useState(null)
